95a1541c7850c5e3099bc15616c17ea894a12063
mc_Edwin <99588600+EdwinYoungSteve@users.noreply.github.com>
2025-11-19 21:01:08 +0800

    增加添加奇术焦镜词条的支持

--- src/main/java/xyz/phanta/tconevo/integration/crafttweaker/CrTClassTconEvo.java
+++ src/main/java/xyz/phanta/tconevo/integration/crafttweaker/CrTClassTconEvo.java
@@ -5,11 +5,13 @@ import crafttweaker.IAction;
 import crafttweaker.annotations.ZenRegister;
 import slimeknights.tconstruct.library.TinkerRegistry;
 import slimeknights.tconstruct.library.materials.Material;
+import slimeknights.tconstruct.library.traits.ITrait;
 import stanhebben.zenscript.annotations.ZenClass;
 import stanhebben.zenscript.annotations.ZenMethod;
 import xyz.phanta.tconevo.init.TconEvoPartTypes;
 import xyz.phanta.tconevo.material.stats.MagicMaterialStats;
 
+import javax.annotation.Nullable;
 import java.util.NoSuchElementException;
 
 @ZenRegister
@@ -18,7 +20,8 @@ public class CrTClassTconEvo {
 
     @ZenMethod
     public static void setMagicMaterialStats(String materialId,
-                                             int durability, float potency, float range, int harvestLevel) {
+                                             int durability, float potency, float range, int harvestLevel,
+                                             @Nullable String[] traitIds) {
         CraftTweakerAPI.apply(new IAction() {
             @Override
             public void apply() {
@@ -33,12 +36,32 @@ public class CrTClassTconEvo {
                     TinkerRegistry.addMaterialStats(
                             mat, new MagicMaterialStats(durability, potency, range, harvestLevel));
                 }
+                
+                // Add traits to the magic part type
+                if (traitIds != null && traitIds.length > 0) {
+                    for (String traitId : traitIds) {
+                        ITrait trait = TinkerRegistry.getTrait(traitId);
+                        if (trait != null) {
+                            if (!mat.hasTrait(traitId, TconEvoPartTypes.MAGIC)) {
+                                mat.addTrait(trait, TconEvoPartTypes.MAGIC);
+                            }
+                        } else {
+                            CraftTweakerAPI.logWarning("Unknown trait: " + traitId + " for material: " + materialId);
+                        }
+                    }
+                }
             }
 
             @Override
             public String describe() {
-                return String.format("Setting magic material stats for %s to {dur=%d,pot=%f,ran=%f,har=%d}",
-                        materialId, durability, potency, range, harvestLevel);
+                StringBuilder desc = new StringBuilder(String.format(
+                        "Setting magic material stats for %s to {dur=%d,pot=%f,ran=%f,har=%d}",
+                        materialId, durability, potency, range, harvestLevel));
+                if (traitIds != null && traitIds.length > 0) {
+                    desc.append(" with traits: ");
+                    desc.append(String.join(", ", traitIds));
+                }
+                return desc.toString();
             }
         });
     }
